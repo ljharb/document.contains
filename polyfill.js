da7586e261f40205bc99117c19fa203611bff4f4
Jordan Harband <ljharb@gmail.com>
2020-06-12 23:16:25 -0700

    [Fix] `polyfill`: avoid `document.contains()` crash in IE 11
    Fixes #11.

--- polyfill.js
+++ polyfill.js
@@ -8,7 +8,11 @@ module.exports = function getPolyfill() {
 			return document.contains;
 		}
 		if (document.body && document.body.contains) {
-			return document.body.contains;
+			try {
+				if (typeof document.body.contains.call(document, '') === 'boolean') {
+					return document.body.contains;
+				}
+			} catch (e) { /**/ }
 		}
 	}
 	return implementation;
